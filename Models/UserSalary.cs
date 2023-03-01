190a36be14e7335adb2f48611d311cabab7c9d46
Yura Abharian <yuraabharian@gmail.com>
2023-03-01 01:41:50 -0800

    add Models

---
+++ Models/UserSalary.cs
@@ -0,0 +1,9 @@
+namespace DotnetApi
+{
+    public partial class UserSalary
+    {
+        public int UserId {get; set;}
+        public decimal Salary {get; set;} = 0;
+        public decimal Average {get; set;} = 0;
+    }
+}
