2da4e3cf2371ab69017360972cc7a4f448871914
Yura Abharian <yuraabharian@gmail.com>
2023-03-02 23:11:58 -0800

    Auth Dtos

---
+++ Dtos/UserForLoginConfirmationDto.cs
@@ -0,0 +1,8 @@
+namespace DotnetApi.Dtos
+{
+    public partial class UserForLoginConfirmationDto
+    {
+        public string Email  {get;set;} = "";
+        public string Password  {get;set;} = "";
+    }
+}
